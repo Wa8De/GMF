30b199bf69e3a57a5eb47e6b5a3f7f83bb31766a
WaDe <morrocan.wade@gmail.com>
2024-03-08 12:36:04 +0000

    hhhh

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -32,7 +32,7 @@ const Home = () => {
           initial={{ opacity: 1 }}
           animate={{ opacity: 1 }}
           src="/assets/coins-cut2.png"
-          className="huge:w-4/12 w-72 absolute lg:w-3/12 huge:-left-56 huge:-top-72 md:rotate-45  left-0 bg-da "
+          className="huge:w-4/12 w-72 absolute lg:w-3/12 huge:-left-56 huge:-top-72 md:rotate-45 top-0  left-0 bg-da "
           alt="left-Coins"
           draggable="false"
           style={{ zIndex: 9998 }}
@@ -41,7 +41,7 @@ const Home = () => {
           initial={{ opacity: 1 }}
           animate={{ opacity: 1 }}
           src="/assets/coins2.png"
-          className="huge:w-4/12 w-72 absolute lg:w-3/12 huge:-right-56 huge:-top-72 md:rotate-45 right-0 "
+          className="huge:w-4/12 w-72 absolute lg:w-3/12 huge:-right-56 huge:-top-72 md:rotate-45  right-0 "
           alt="right-Coins"
           draggable="false"
           // style={{ zIndex: 9998 }}
